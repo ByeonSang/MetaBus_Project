1ec10d28e87054a177b9501bee79ad03bcd3652d
ByeonSang <85797062+ByeonSang@users.noreply.github.com>
2025-02-17 20:09:08 +0900

    Feat: FollowCamera 최소, 최대 이동가능한 거리 추가
    Min Move
    Max Move
    설정

--- Assets/03.Scripts/CameraMovement.cs
+++ Assets/03.Scripts/CameraMovement.cs
@@ -8,9 +8,16 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private Transform target;
     [SerializeField] private float followSpeed;
 
+    [Space]
+    [SerializeField] private Vector2 minDistance;
+    [SerializeField] private Vector2 maxDistance;
     private void Update()
     {
         Vector2 pos = Vector2.Lerp(transform.position, target.position, followSpeed * Time.deltaTime);
+
+        pos.x = Mathf.Clamp(pos.x, minDistance.x, maxDistance.x);
+        pos.y = Mathf.Clamp(pos.y, minDistance.y, maxDistance.y);
+
         transform.position = new Vector3(pos.x, pos.y, transform.position.z);
     }
 }
